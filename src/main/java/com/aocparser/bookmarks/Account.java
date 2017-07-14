72f6dc34a39c0c4e1bf89f4f832f8f1f811014ef
Artem Krotov <artem.krotov@eldorado.ru>
2017-07-14 16:56:44 +0300

    aster

---
+++ src/main/java/com/aocparser/bookmarks/Account.java
@@ -0,0 +1,39 @@
+package com.aocparser.bookmarks;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.Data;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Data
+public class Account {
+
+    @OneToMany(mappedBy = "account")
+    private Set<Bookmark> bookmarks = new HashSet<>();
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    public Set<Bookmark> getBookmarks() {
+        return bookmarks;
+    }
+
+    @JsonIgnore
+    public String password;
+    public String username;
+
+    public Account(String name, String password) {
+        this.username = name;
+        this.password = password;
+    }
+
+    Account() { // jpa only
+    }
+}
---
+++ src/main/java/com/aocparser/bookmarks/Bookmark.java
@@ -0,0 +1,37 @@
+package com.aocparser.bookmarks;
+
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.Data;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+
+@Entity
+@Data
+public class Bookmark {
+
+    @JsonIgnore
+    @ManyToOne
+    private Account account;
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    Bookmark() { // jpa only
+    }
+
+    public Bookmark(Account account, String uri, String description) {
+        this.uri = uri;
+        this.description = description;
+        this.account = account;
+    }
+
+    public String uri;
+    public String description;
+
+
+}
